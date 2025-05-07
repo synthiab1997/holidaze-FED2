07920dff8bf2c0321e8558f0919235055bfa1208
synthiab1997 <149474030+synthiab1997@users.noreply.github.com>
2025-05-07 15:41:17 +0200

    tailwind+vite setup

---
+++ holidaze-frontend/tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+module.exports = {
+    content: [
+      './index.html',
+      './src/**/*.{js,jsx,ts,tsx}', // Tailwind will scan these files for classes
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
